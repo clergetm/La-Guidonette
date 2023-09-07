c65cf51726d09fafb3cc3fa2e2de0b02ea37d5cf
davidge <geoffrey.david@univ-grenoble-alpes.fr>
2023-09-07 16:40:49 +0200

    refactor: entities generation based on data.jdl file

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,21 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'product',
+        data: { pageTitle: 'laGuidonetteApp.product.home.title' },
+        loadChildren: () => import('./product/product.module').then(m => m.ProductModule),
+      },
+      {
+        path: 'category',
+        data: { pageTitle: 'laGuidonetteApp.category.home.title' },
+        loadChildren: () => import('./category/category.module').then(m => m.CategoryModule),
+      },
+      {
+        path: 'torder',
+        data: { pageTitle: 'laGuidonetteApp.torder.home.title' },
+        loadChildren: () => import('./torder/torder.module').then(m => m.TorderModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
