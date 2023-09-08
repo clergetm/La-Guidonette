b8080a393e78639dbd163584ba94973d7e239e85
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-08 12:04:18 +0200

    Feat: Added necessary modules to use Angular Materials

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -25,15 +25,8 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { HeaderComponent } from './layouts/header/header.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { ListProductsComponent } from './list-products/list-products.component';
-import { ProductCardComponent } from './layouts/product-card/product-card.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { CartComponent } from './cart/cart.component';
-import { OrderComponent } from './order/order.component';
-import { PaymentComponent } from './payment/payment.component';
-import { StepperAnimationComponent } from './layouts/stepper-animation/stepper-animation.component';
 
 @NgModule({
   imports: [
@@ -48,9 +41,6 @@ import { StepperAnimationComponent } from './layouts/stepper-animation/stepper-a
     NgxWebstorageModule.forRoot({ prefix: 'jhi', separator: '-', caseSensitive: true }),
     TranslationModule,
     MatSlideToggleModule,
-    MatCardModule,
-    MatButtonModule,
-    MatButtonModule,
   ],
   providers: [
     Title,
@@ -65,15 +55,9 @@ import { StepperAnimationComponent } from './layouts/stepper-animation/stepper-a
     PageRibbonComponent,
     ActiveMenuDirective,
     FooterComponent,
-    ProductCardComponent,
-    ListProductsComponent,
-    CartComponent,
-    OrderComponent,
-    PaymentComponent,
-    StepperAnimationComponent,
+    HeaderComponent,
   ],
   bootstrap: [MainComponent],
-  exports: [ProductCardComponent],
 })
 export class AppModule {
   constructor(applicationConfigService: ApplicationConfigService, iconLibrary: FaIconLibrary, dpConfig: NgbDatepickerConfig) {
