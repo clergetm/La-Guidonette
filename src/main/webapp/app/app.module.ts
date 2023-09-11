43d6e851919ac6bbf003787730e85298809ca1fc
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-11 15:03:59 +0200

    Feat: Add modules used in updated components, particularly angular material modules

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -25,6 +25,12 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { ListProductsComponent } from './list-products/list-products.component';
+import { ProductCardComponent } from './layouts/product-card/product-card.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   imports: [
@@ -38,6 +44,9 @@ import { ErrorComponent } from './layouts/error/error.component';
     HttpClientModule,
     NgxWebstorageModule.forRoot({ prefix: 'jhi', separator: '-', caseSensitive: true }),
     TranslationModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [
     Title,
@@ -45,7 +54,7 @@ import { ErrorComponent } from './layouts/error/error.component';
     { provide: NgbDateAdapter, useClass: NgbDateDayjsAdapter },
     httpInterceptorProviders,
   ],
-  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent, ListProductsComponent, ProductCardComponent],
   bootstrap: [MainComponent],
 })
 export class AppModule {
