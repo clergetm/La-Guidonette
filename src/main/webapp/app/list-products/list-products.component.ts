6c36df41194a370b6aa99c8023d10b453e4a3d48
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-11 09:58:11 +0200

    Feat: Add a page listing the products. This could be a home, or a filter page later on.

---
+++ src/main/webapp/app/list-products/list-products.component.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+
+import { Product, products } from './product';
+import { CartContentService } from '../cart-content.service';
+
+@Component({
+  selector: 'jhi-list-products',
+  templateUrl: './list-products.component.html',
+  styleUrls: ['./list-products.component.scss'],
+})
+export class ListProductsComponent implements OnInit {
+  products = [...products];
+  constructor(private cartService: CartContentService) {}
+
+  ngOnInit(): void {}
+}
