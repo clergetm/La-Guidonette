09520753a44e24f0f48e3fc01f6a8e57de112543
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-11 15:22:31 +0200

    Feat: Add a stepper to make step-by-step forms, currently empty

---
+++ src/main/webapp/app/layouts/stepper-animation/stepper-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-stepper-animation',
+  templateUrl: './stepper-animation.component.html',
+  styleUrls: ['./stepper-animation.component.scss']
+})
+export class StepperAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
