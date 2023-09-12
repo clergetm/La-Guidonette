084371e9f8291587f87c782d05b3834a63e250af
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-12 09:33:23 +0200

    Fix: Remove unused

--- src/main/webapp/app/payment/payment.module.ts
+++ src/main/webapp/app/payment/payment.module.ts
@@ -3,7 +3,6 @@ import { RouterModule } from '@angular/router';
 
 import { SharedModule } from 'app/shared/shared.module';
 import { PAYMENT_ROUTE } from './payment.route';
-import {StepperAnimationComponent} from "../layouts/stepper-animation/stepper-animation.component";
 import {StepperAnimationModule} from "../layouts/stepper-animation/stepper-animation.module";
 import {PaymentComponent} from "./payment.component";
 
