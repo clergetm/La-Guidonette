c98ed4d6ab0cc436e227e087f9f049b6117226fb
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-12 16:55:21 +0200

    Feat: Get the product description and display it, through the static products list

--- src/main/webapp/app/product-page/product-page.component.ts
+++ src/main/webapp/app/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {getProductById, Product, products} from "../list-products/product";
 
 @Component({
   selector: 'jhi-product-page',
@@ -8,10 +9,23 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ProductPageComponent implements OnInit {
   productId: string | null = "-1"
+  product: Product | null = {
+    id: -1,
+    price: '233',
+    name: '',
+    brand: '',
+    mediaUrl: '',
+    stock: -1,
+    color: '',
+    description: '',
+  };
   constructor(public route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.productId = this.route.snapshot.paramMap.get('id');
+    if (typeof this.productId === "string") {
+      this.product = getProductById(parseInt(this.productId));
+    }
   }
 
 }
