747d353ceb814906763e5e19f165f268e80bfc65
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-12 14:59:54 +0200

    Feat: Created product-page

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -27,6 +27,7 @@ import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @NgModule({
   imports: [
@@ -56,6 +57,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     ActiveMenuDirective,
     FooterComponent,
     HeaderComponent,
+    ProductPageComponent,
   ],
   bootstrap: [MainComponent],
 })
