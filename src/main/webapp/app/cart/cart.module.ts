f69423c1cfdeff5325e276be1b9ffff136957a28
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-12 09:58:44 +0200

    Fix: order page broken

--- src/main/webapp/app/cart/cart.module.ts
+++ src/main/webapp/app/cart/cart.module.ts
@@ -3,8 +3,11 @@ import { RouterModule } from '@angular/router';
 
 import { SharedModule } from 'app/shared/shared.module';
 import { CART_ROUTE } from './cart.route';
+import {CartComponent} from "./cart.component";
 
 @NgModule({
+  declarations: [CartComponent],
   imports: [SharedModule, RouterModule.forChild([CART_ROUTE])],
+  exports: [CartComponent]
 })
 export class CartModule {}
