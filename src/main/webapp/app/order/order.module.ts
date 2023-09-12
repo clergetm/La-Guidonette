7f4bd4e824128773ebdaed0f9a69de641a68c5fc
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-12 09:58:44 +0200

    Fix: order page broken

--- src/main/webapp/app/order/order.module.ts
+++ src/main/webapp/app/order/order.module.ts
@@ -3,8 +3,11 @@ import { RouterModule } from '@angular/router';
 
 import { SharedModule } from 'app/shared/shared.module';
 import { ORDER_ROUTE } from './order.route';
+import {OrderComponent} from "./order.component";
 
 @NgModule({
+  declarations: [OrderComponent],
   imports: [SharedModule, RouterModule.forChild([ORDER_ROUTE])],
+  exports: [OrderComponent]
 })
 export class OrderModule {}
