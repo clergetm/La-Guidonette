566af5a911ba4ec365c7543a59fd1c33866b29c1
folet14 <jed14@orange.fr>
2023-09-13 15:36:13 +0200

    feat: add base product page

--- src/main/webapp/app/product-page/product-page.component.ts
+++ src/main/webapp/app/product-page/product-page.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {getProductById, Product, products} from "../list-products/product";
+import {CartContentService} from '../cart-content.service';
 
 @Component({
   selector: 'jhi-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.scss']
+
 })
 export class ProductPageComponent implements OnInit {
   productId: string | null = "-1"
@@ -19,12 +21,24 @@ export class ProductPageComponent implements OnInit {
     color: '',
     description: '',
   };
-  constructor(public route: ActivatedRoute) { }
+
+  constructor(public route: ActivatedRoute, private cartService: CartContentService) {
+  }
 
   ngOnInit(): void {
     this.productId = this.route.snapshot.paramMap.get('id');
     if (typeof this.productId === "string") {
       this.product = getProductById(parseInt(this.productId));
+    }
+  }
+
+  addToCart(): void {
+    if (this.product != null) {
+      var numberSelector: any;
+      numberSelector = document.getElementById("numberOfItem");
+      if (numberSelector != null) {
+        this.cartService.addMultipleToCart(this.product, numberSelector.value);
+      }
     }
   }
 
