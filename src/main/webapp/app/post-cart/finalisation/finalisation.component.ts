1ffcb9446f2cc8f5ffb86e9da50b79063462bdc6
Matvei <32172257+DrankRock@users.noreply.github.com>
2023-09-14 11:00:25 +0200

    [FEATURE] Front end confirm cart (#26)
    * Fix: Add a spacing between navbar and the rest of the page
    
    * Feat: Started the payment
    
    * Feat: Not connected user can't access the order page
    
    * Feat: Added a post-cart component to use all the podt-cart components. Have it set to auto redirect to login if user is not logged in.
    
    * Feat: Added a finalisation component to thank the user for the order
    
    * Removed the components only used as sub-components
    
    * Feat: added a getPrice() function to get the total price of the cart
    
    * Feat: added finalisation, payment and order recapitulation to the page, plus next/  previous buttons, and overall basic functions
    
    * Feat: Added payments input with some typescript magic to make it look good
    
    * Feat: added finalisation, payment and order recapitulation to the page, plus next/  previous buttons, and overall basic functions
    
    * Fix: api/products** available without being logged in
    
    * Mv: Moved the cart service to a specific services folder
    
    * Mv: Moved cart content service to services folder
    
    * Mv: update path of cart content service
    
    * Fix: Inputs in the payment page now only accept numbers + fixing some display issues, and adding a back to home button after the finalisation

---
+++ src/main/webapp/app/post-cart/finalisation/finalisation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-finalisation',
+  templateUrl: './finalisation.component.html',
+  styleUrls: ['./finalisation.component.scss']
+})
+export class FinalisationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
