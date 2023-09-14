c3e0957db9eb600e113aa132bca16eb95af5dd37
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-14 14:12:47 +0200

    [FEATURE] Add Filter menu (#53) (#41)
    feat: Add filter menu
    
    Feat: Initial commit
    
    Feat: Started the navbar, trying to fix the issue where it's floating over the other instead of being left to them
    
    Feat: Categories are accessible by everyone
    
    Feat: Adapt the navbar to the page it is on, and to the main content div in term of size
    
    Feat: Add a sidebar and fill it with elements from the db
    
    Fix: Sidebar is not hidden behind navbar anymore
    
    Feat: add the script to kill background h2 java in the .gitignore
    
    Doc: Added clarity spaces that were missing in imports
    
    Doc: Added clarity spaces
    
    fix: remove spaces
    
    chore: remove spaces
    
    chore: format imports
    
    Fix: Show sidebar on products* and not just products
    
    Feat: Link a category to /products?category=name to later create a search from that
    
    Feat: click on category does a search
    
    fix: frontend sidebar styling
    
    feature: arrow see less + see more
    
    feature: filtered search on backend
    
    feature: filter and search page
    
    fix: put search function in product service

--- src/main/java/fr/uga/laguidonette/repository/ProductRepository.java
+++ src/main/java/fr/uga/laguidonette/repository/ProductRepository.java
@@ -4,10 +4,10 @@ import fr.uga.laguidonette.domain.Product;
 import fr.uga.laguidonette.domain.enumeration.Brand;
 import fr.uga.laguidonette.domain.enumeration.Color;
 import java.util.List;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -32,11 +32,24 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     );
 
     @Query(
+        value = "select distinct p from Product p " +
+        "left join p.categories c " +
+        "where (lower(p.label) like lower(concat('%', :query,'%')) or lower(p.brand) like lower(concat('%', :query,'%')))" +
+        "and (c.name in :categories or p.color in :colors  or  p.brand in :brands) "
+    )
+    Page<Product> filteredSearch(
+        @Param("query") String name,
+        @Param("categories") List<String> categories,
+        @Param("colors") List<Color> colors,
+        @Param("brands") List<Brand> brands,
+        Pageable pageable
+    );
+
+    @Query(
         value = "select p as product, sum(ol.quantity) as productOrderQuantity " +
         " from OrderLine ol inner join ol.product p" +
         " group by p.id" +
         " order by productOrderQuantity desc"
     )
     List<Product> getBestSellers(Pageable pageable);
-
 }
