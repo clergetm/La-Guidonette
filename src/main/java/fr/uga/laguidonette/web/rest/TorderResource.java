e9d2cc7eef1147b0803d357ff81cc3fa04dd232d
Polluxin <geoffrey.david@orange.fr>
2023-09-14 23:20:17 +0200

    feature: add PostOrderDto used to create an order.
    feature: add createTorderFromProducts in TorderResource (back), not tested, not launched

--- src/main/java/fr/uga/laguidonette/web/rest/TorderResource.java
+++ src/main/java/fr/uga/laguidonette/web/rest/TorderResource.java
@@ -1,14 +1,23 @@
 package fr.uga.laguidonette.web.rest;
 
+import fr.uga.laguidonette.domain.OrderLine;
+import fr.uga.laguidonette.domain.Product;
 import fr.uga.laguidonette.domain.Torder;
+import fr.uga.laguidonette.repository.ProductRepository;
 import fr.uga.laguidonette.repository.TorderRepository;
+import fr.uga.laguidonette.service.OrderLineService;
+import fr.uga.laguidonette.service.ProductService;
 import fr.uga.laguidonette.service.TorderService;
+import fr.uga.laguidonette.service.dto.PostOrderDto;
 import fr.uga.laguidonette.web.rest.errors.BadRequestAlertException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import javax.persistence.OptimisticLockException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import org.slf4j.Logger;
@@ -37,19 +46,32 @@ public class TorderResource {
 
     private final TorderRepository torderRepository;
 
-    public TorderResource(TorderService torderService, TorderRepository torderRepository) {
+    private final ProductService productService;
+
+    private final ProductRepository productRepository;
+
+    private final OrderLineService orderLineService;
+
+    public TorderResource(
+        TorderService torderService,
+        TorderRepository torderRepository,
+        ProductRepository productRepository,
+        ProductService productService,
+        OrderLineService orderLineService
+    ) {
         this.torderService = torderService;
         this.torderRepository = torderRepository;
+        this.productRepository = productRepository;
+        this.productService = productService;
+        this.orderLineService = orderLineService;
     }
 
     /**
-     * {@code POST  /torders} : Create a new torder.
      *
      * @param torder the torder to create.
      * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new torder, or with status {@code 400 (Bad Request)} if the torder has already an ID.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
-    @PostMapping("/torders")
     public ResponseEntity<Torder> createTorder(@Valid @RequestBody Torder torder) throws URISyntaxException {
         log.debug("REST request to save Torder : {}", torder);
         if (torder.getId() != null) {
@@ -60,6 +82,64 @@ public class TorderResource {
             .created(new URI("/api/torders/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
             .body(result);
+    }
+
+    /**
+     * {@code POST  /torders} : Create a new torder using a list of orderlines and a user.
+     */
+    @PostMapping("/torders")
+    public ResponseEntity<Torder> createTorderFromProducts(@Valid @RequestBody PostOrderDto postOrderDto) throws URISyntaxException {
+        log.debug("REST request to save Torder from products : {}", postOrderDto);
+        if (postOrderDto.getOrderLines() == null) {
+            throw new BadRequestAlertException("A new torder cannot have an empty orderlines list", "product", "noorderline");
+        }
+        if (postOrderDto.getUser() == null) {
+            throw new BadRequestAlertException("A new torder must have an user", "user", "nouser");
+        }
+        // Step 1: Verify that each product of each orderline has a stock >= orderline.quantity
+        for (OrderLine orderLine : postOrderDto.getOrderLines()) {
+            if (orderLine.getProduct() == null) {
+                throw new BadRequestAlertException("A new orderline must have a product", "product", "noproducts");
+            }
+            if (!productRepository.existsById(orderLine.getProduct().getId())) {
+                throw new BadRequestAlertException("Entity not found", "product", "idnotfound");
+            }
+
+            Optional<Product> optionalProduct = productService.findOne(orderLine.getProduct().getId());
+            if (optionalProduct.get().getQuantity() < orderLine.getQuantity()) throw new BadRequestAlertException(
+                "Product " + optionalProduct.get().getId() + "has a quantity of " + optionalProduct.get().getQuantity(),
+                "product",
+                "badquantity"
+            );
+        }
+        // Step 2: Decrement stock by quantity for each product
+        for (OrderLine orderLine : postOrderDto.getOrderLines()) {
+            orderLine.getProduct().setQuantity(orderLine.getQuantity());
+            try {
+                productService.update(orderLine.getProduct());
+            } catch (OptimisticLockException e) {
+                Optional<Product> optionalProduct = productService.findOne(orderLine.getProduct().getId());
+                optionalProduct.get().setQuantity(orderLine.getQuantity());
+                productService.update(optionalProduct.get());
+            }
+        }
+        // Step 3: Create a new Torder
+        Torder torder = new Torder();
+        torder.id(null);
+        torder.userID(postOrderDto.getUser());
+        torder.date(Instant.now());
+        Torder createdTorder = createTorder(torder).getBody();
+        // Step 4: Create each Orderline linked with the torder and the user.
+        for (OrderLine orderLine : postOrderDto.getOrderLines()) {
+            orderLine.setTorder(createdTorder);
+            orderLineService.save(orderLine);
+        }
+
+        assert createdTorder != null;
+        return ResponseEntity
+            .created(new URI("/api/torders/" + createdTorder.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, createdTorder.getId().toString()))
+            .body(createdTorder);
     }
 
     /**
