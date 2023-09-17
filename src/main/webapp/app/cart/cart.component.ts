90b0b1e378164ce251bbd7986336e3474a2361bf
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-17 15:35:12 +0200

    Feat: Continue doing the styling, the font looks like something, a theorized sidebar coloring exists

--- src/main/webapp/app/cart/cart.component.ts
+++ src/main/webapp/app/cart/cart.component.ts
@@ -2,18 +2,15 @@ import { Component } from '@angular/core';
 import { CartContentService } from '../services/cart-content.service';
 
 import { IProduct } from '../entities/product/product.model';
-import {AccountService} from "../core/auth/account.service";
+import { AccountService } from '../core/auth/account.service';
 
 @Component({
   selector: 'jhi-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss'],
+  styleUrls: ['../styles.scss'],
 })
 export class CartComponent {
-  constructor(
-    public cartService: CartContentService,
-    public account: AccountService
-  ) {}
+  constructor(public cartService: CartContentService, public account: AccountService) {}
 
   deleteCart(): void {
     this.cartService.removeAll();
