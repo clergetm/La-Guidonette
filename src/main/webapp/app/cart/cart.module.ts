42966c28050ff8638f0971ec20e01e2c6c8aa306
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-17 10:58:09 +0200

    [FEATURE] Add CSS (#64)
    Feat: Continue doing the styling, the font looks like something, a theorized sidebar coloring exists
    
    feat: continue styling of cart
    
    feat: continue styling, cart almost done, requires a rebase of dev
    
    feat: Cart done + Added a function in cartcontentservice
    
    feat: Added styling for the table. Added a total for each line. Modified buttons.
    
    feat: Added security in the payment page with luhn algorithm and length check + date compared to today for expiration
    
    feat: Finished payment as a responsive form
    
    feat: Fix some rebase issues and removed a problematic css import
    
    feat: Added multiple error messages, fixed some gramatical error, added some more blocks.
    
    feat: Added informations under inputs in payment to know why the number is invalid.
    
    feat: Started to remove the alert stuff, because if i check without pressing command, the alert never opens
    
    feat: Removed the alert for paymetn that is never called + info on what's wrong show up only when modifying the concerned input
    
    feat: put back navbar as it was before, modified the position of the number of items in cart, modified the size of the header in payment and finalisation, removed retour from the Final post-cart page
    
    fix: increased max capacity for a style element
    
    fix: removed image in cart because it causes bug issues when contacting aws
    
    fix: remove version number
    
    fix: unnecessarily long if else and unused variables

--- src/main/webapp/app/cart/cart.module.ts
+++ src/main/webapp/app/cart/cart.module.ts
@@ -3,11 +3,12 @@ import { RouterModule } from '@angular/router';
 
 import { SharedModule } from 'app/shared/shared.module';
 import { CART_ROUTE } from './cart.route';
-import {CartComponent} from "./cart.component";
+import { CartComponent } from './cart.component';
+import { ProductCardModule } from '../layouts/product-card/product-card.module';
 
 @NgModule({
   declarations: [CartComponent],
-  imports: [SharedModule, RouterModule.forChild([CART_ROUTE])],
-  exports: [CartComponent]
+  imports: [SharedModule, RouterModule.forChild([CART_ROUTE]), ProductCardModule],
+  exports: [CartComponent],
 })
 export class CartModule {}
--- src/main/webapp/app/post-cart/payment/payment.component.ts
+++ src/main/webapp/app/post-cart/payment/payment.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../../core/auth/account.service';
 import { CartContentService } from '../../services/cart-content.service';
 
 @Component({
   selector: 'jhi-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.scss', '../post-cart.component.scss'],
+  styleUrls: ['./payment.component.scss', '../../styles.scss'],
 })
 export class PaymentComponent {
-  step = 1;
+  @Output() canGoNext = new EventEmitter<boolean>();
 
   cardNumber = '';
   expiryDate = '';
-  cvv = '';
+  cvv: string = '';
+  name: string = '';
+
+  validInputs: boolean[] = [false, false, false, false]; // cc, date, cvv, name
   constructor(public accountService: AccountService, public cartContentService: CartContentService) {}
+
+  error_name = false;
+  error_card_short = false;
+  error_card_long = false;
+  error_cvv = false;
+  error_date_incorrect = false;
+  error_date_expired = false;
+  error_date_format = false;
 
   formatCardNumber(): void {
     const formatted = this.cardNumber.replace(/\D/g, '').match(/.{1,4}/g);
     this.cardNumber = formatted ? formatted.join(' ') : '';
+    this.isValidCCNumber();
+    this.checkCanGoNext();
   }
 
   formatExpiryDate(): void {
@@ -26,9 +39,59 @@ export class PaymentComponent {
     if (this.expiryDate.length > 5) {
       this.expiryDate = this.expiryDate.substr(0, 5);
     }
+    this.isValidDate();
+    this.checkCanGoNext();
   }
 
   formatCVV(): void {
     this.cvv = this.cvv.replace(/\D/g, '');
+    this.error_cvv = !(this.cvv.length === 3);
+    this.validInputs[2] = !this.error_cvv;
+    this.checkCanGoNext();
+  }
+
+  formatName(): void {
+    this.name = this.name.replace(/[0-9]/g, '');
+    this.error_name = !(this.name.length > 0);
+    this.validInputs[3] = !this.error_name;
+    this.checkCanGoNext();
+  }
+
+  checkCanGoNext() {
+    const isValid = this.validInputs[0] && this.validInputs[1] && this.validInputs[2] && this.validInputs[3];
+    this.canGoNext.emit(isValid);
+  }
+
+  /**
+   * Use Luhn algorithm to find if the credit card number is valid
+   * Algo from https://decipher.dev/30-seconds-of-typescript/docs/luhnCheck/
+   */
+  isValidCCNumber(): boolean {
+    const noSpace = this.cardNumber.replace(/\s/g, '');
+    this.error_card_short = noSpace.length < 13;
+    this.error_card_long = noSpace.length > 19;
+    const retour = !this.error_card_long && !this.error_card_short;
+    this.validInputs[0] = retour;
+    return retour;
+  }
+
+  isValidDate(): boolean {
+    this.error_date_format = !(this.expiryDate.length === 5);
+    if (this.error_date_format) {
+      return false;
+    }
+    const month = parseInt(this.expiryDate.split('/')[0]);
+    const year = parseInt(this.expiryDate.split('/')[1]);
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear() % 100; // Get the last two digits of the year
+    const currentMonth = currentDate.getMonth() + 1; // Months are 0-indexed in JS
+
+    this.error_date_incorrect = month < 1 || month > 12;
+    this.error_date_expired = !(currentYear * 12 + currentMonth <= year * 12 + month);
+
+    let retour = !this.error_date_expired && !this.error_date_format && !this.error_date_incorrect;
+    this.validInputs[1] = retour;
+    return retour;
   }
 }
