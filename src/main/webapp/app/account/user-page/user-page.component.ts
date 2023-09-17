9d6e954c2e3b4d7a087dde9f77f1bcc65caf4665
Mathys Clerget <mathysclerget+github@gmail.com>
2023-09-17 23:00:47 +0200

    chore: linting user-page

--- src/main/webapp/app/account/user-page/user-page.component.ts
+++ src/main/webapp/app/account/user-page/user-page.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'jhi-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.scss']
+  styleUrls: ['./user-page.component.scss'],
 })
 export class UserPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
