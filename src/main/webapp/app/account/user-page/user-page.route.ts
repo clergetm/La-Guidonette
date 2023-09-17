feb139edbea97db7e50a895b7e1b1ad51ae5ed5b
Mathys Clerget <mathysclerget+github@gmail.com>
2023-09-17 23:00:47 +0200

    chore: linting user-page

--- src/main/webapp/app/account/user-page/user-page.route.ts
+++ src/main/webapp/app/account/user-page/user-page.route.ts
@@ -1,7 +1,7 @@
-import {Route} from '@angular/router';
+import { Route } from '@angular/router';
 
-import {UserRouteAccessService} from 'app/core/auth/user-route-access.service';
-import {UserPageComponent} from './user-page.component';
+import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
+import { UserPageComponent } from './user-page.component';
 
 export const userPageRoute: Route = {
   path: 'user-page',
