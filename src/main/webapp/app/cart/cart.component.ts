67edb39051f1201b1d66054d3a6d80be054a1ab9
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-18 16:03:38 +0200

    Feat: Cart done + Added a function in cartcontentservice

--- src/main/webapp/app/cart/cart.component.ts
+++ src/main/webapp/app/cart/cart.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartContentService } from '../services/cart-content.service';
 
 import { IProduct } from '../entities/product/product.model';
 import { AccountService } from '../core/auth/account.service';
+import { CoupleProductQuantity } from '../entities/dto/CoupleProductQuantity';
 
 @Component({
   selector: 'jhi-cart',
@@ -11,12 +12,14 @@ import { AccountService } from '../core/auth/account.service';
 })
 export class CartComponent {
   constructor(public cartService: CartContentService, public account: AccountService) {}
-
   deleteCart(): void {
     this.cartService.removeAll();
   }
 
   deleteItem(prod: IProduct): void {
     this.cartService.removeFromCart(prod);
+  }
+  addItem(prod: IProduct): void {
+    this.cartService.addToCart(prod);
   }
 }
