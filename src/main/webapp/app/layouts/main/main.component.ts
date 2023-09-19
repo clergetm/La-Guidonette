0b4f4101ea7e4e651c8d7b2229f83328501b06fc
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-19 12:10:48 +0200

    Feat: Fix some rebase issues and removed a problematic css import

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -9,7 +9,7 @@ import { AccountService } from 'app/core/auth/account.service';
 @Component({
   selector: 'jhi-main',
   templateUrl: './main.component.html',
-  styleUrls: ['../..//styles.scss'],
+  styleUrls: ['../../styles.scss'],
 })
 export class MainComponent implements OnInit {
   private renderer: Renderer2;
