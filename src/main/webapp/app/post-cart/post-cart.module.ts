498b890639c4c5c3adf3b57c57b6442a57833f56
Polluxin <77778935+Polluxin@users.noreply.github.com>
2023-09-19 09:57:16 +0200

    [BUGFIX] Navigation post cart (#66)
    Co-authored-by: Matthys

--- src/main/webapp/app/post-cart/post-cart.module.ts
+++ src/main/webapp/app/post-cart/post-cart.module.ts
@@ -6,7 +6,6 @@ import { POSTCART_ROUTE } from './post-cart.route';
 import { PostCartComponent } from './post-cart.component';
 import { OrderModule } from './order/order.module';
 import { PaymentModule } from './payment/payment.module';
-import { FinalisationComponent } from './finalisation/finalisation.component';
 import { FinalisationModule } from './finalisation/finalisation.module';
 
 @NgModule({
