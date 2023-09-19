ce30e0eafd87ec8f5cf867b9015d748e12a744d1
davidge <geoffrey.david@univ-grenoble-alpes.fr>
2023-09-19 14:36:27 +0200

    feature: style productPage

--- src/main/webapp/app/layouts/product-card/product-card.module.ts
+++ src/main/webapp/app/layouts/product-card/product-card.module.ts
@@ -10,20 +10,8 @@ import { ProductImageComponent } from '../product-image/product-image.component'
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    ProductCardComponent,
-    ProductImageComponent,
-  ],
-  exports: [
-    ProductCardComponent
-  ],
-  imports: [
-    CommonModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    RouterLink,
-    RouterLinkActive,
-  ],
+  declarations: [ProductCardComponent, ProductImageComponent],
+  exports: [ProductCardComponent, ProductImageComponent],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatToolbarModule, RouterLink, RouterLinkActive],
 })
 export class ProductCardModule {}
