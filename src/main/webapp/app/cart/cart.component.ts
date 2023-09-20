28ad5d79cf564be929b723700c7cd70ca29ceff9
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-09-20 09:54:06 +0200

    [BUGFIX] Auto redirect to order after login (#90)

--- src/main/webapp/app/cart/cart.component.ts
+++ src/main/webapp/app/cart/cart.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CartContentService } from '../services/cart-content.service';
 
 import { IProduct } from '../entities/product/product.model';
 import { AccountService } from '../core/auth/account.service';
+import { StateStorageService } from '../core/auth/state-storage.service';
+import { Router } from '@angular/router';
 import { CoupleProductQuantity } from '../entities/dto/CoupleProductQuantity';
 
 @Component({
@@ -11,13 +13,28 @@ import { CoupleProductQuantity } from '../entities/dto/CoupleProductQuantity';
   styleUrls: ['../styles.scss'],
 })
 export class CartComponent {
-  constructor(public cartService: CartContentService, public account: AccountService) {}
+  constructor(
+    public cartService: CartContentService,
+    public account: AccountService,
+    private stateStorageService: StateStorageService,
+    private router: Router
+  ) {}
+
   deleteCart(): void {
     this.cartService.removeAll();
   }
 
   deleteItem(prod: IProduct): void {
     this.cartService.removeFromCart(prod);
+  }
+
+  redirectToLogin(): void {
+    if (this.account.isAuthenticated()){
+      this.router.navigate(['order']);
+    } else {
+      this.stateStorageService.storeUrl('order');
+      this.router.navigate(['login']);
+    }
   }
   addItem(prod: IProduct): void {
     this.cartService.addToCart(prod);
